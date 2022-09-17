fd544b2405c346851a86c604f327b14cfda235d7
marcelo dominguez <marceelodominguez@gmail.com>
2022-09-17 17:17:00 -0300

    firebase

--- config/firebase.js
+++ config/firebase.js
@@ -4,12 +4,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getReactNativePersistence } from "firebase/auth/react-native";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBRsL3RT9887ycZtgHeAORhBGhnmsMcJa0",
-  authDomain: "react-auth-a4f15.firebaseapp.com",
-  projectId: "react-auth-a4f15",
-  storageBucket: "react-auth-a4f15.appspot.com",
-  messagingSenderId: "985520348970",
-  appId: "1:985520348970:web:c4d4f847357347c7800853",
+  //credenciales de firebase
 };
 
 // Initialize Firebase
